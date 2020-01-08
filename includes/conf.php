a11c470c4bd28f52f5a724bde75ed874a92d9022
Moreno Zolfo <moreno.zolfo@gmail.com>
2020-01-08 14:22:21 +0100

    cleanup
    Former-commit-id: b963ee82848da9ba0b004b5a58c46933562ea7f5

---
+++ includes/conf.php
@@ -0,0 +1,13 @@
+<?php
+
+$db_Host = "localhost";
+$db_User = "drfcmicd_fedusr";
+$db_Pass = '=v!8X7CXox;q'; 
+$db_Name = "drfcmicd_tnf";
+
+
+// $db_Host = "127.0.0.1";
+// $db_User = "root";
+// $db_Pass = "";
+// $db_Name = "gamma"; 
+ 

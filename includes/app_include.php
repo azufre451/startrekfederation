c401b13d04bd213eee647c966c242f3a82ec9cab
Moreno <moreno.zolfo@gmail.com>
2020-04-17 03:29:39 +0200

    dev version

--- includes/app_include.php
+++ includes/app_include.php
@@ -10,17 +10,15 @@ date_default_timezone_set('Europe/Rome');
 //echo date_default_timezone_get();
 
 error_reporting(E_ALL ^ E_DEPRECATED);
-  
+
 Database::tdbConnect($db_Host,$db_User, $db_Pass,$db_Name);
 Database::query('SET NAMES utf8');
-//$formats = array(".jpg",".png",".gif",".bmp");
-//$baseSite = "http://genesis.digital-destiny.org/SigmaSys/";
 
-$gameName = "v. 1.8.6 - Triumphal Zebrafish";
-$gameVersion = "1.8.6 - Triumphal Zebrafish";
+$gameName = "v. 1.8.7 - Nymphomaniac Nightingale";
+$gameVersion = "1.8.7 - Nymphomaniac Nightingale";
 //1.8 Rampant Unicorn
 
-$gameOptions = array('numericVersion' => "1.8.68");
+$gameOptions = array('numericVersion' => "1.8.7");
 
 $debug=true;
 $tips=true;
@@ -31,12 +29,12 @@ $bounceYear = 379;
 
 $gameServiceInfo = 0;
 
-$curTime = time();		
-$lapConstant = ((date("Y",$curTime) % 4) == 0) ? 0.00003162315 : 0.00003170979; 
+$curTime = time();
+$lapConstant = ((date("Y",$curTime) % 4) == 0) ? 0.00003162315 : 0.00003170979;
 //$currentDate = date("d/m",$curTime)."/".(date("Y",$curTime)+368);
-$currentDate = utf8_encode(ucfirst(strftime('%A %e %B %Y',mktime(date("H"),date("i"),date("s"),date("n",$curTime),date("j",$curTime),($bounceYear+date("Y",$curTime))))));	
+$currentDate = utf8_encode(ucfirst(strftime('%A %e %B %Y',mktime(date("H"),date("i"),date("s"),date("n",$curTime),date("j",$curTime),($bounceYear+date("Y",$curTime))))));
 
-//<span class="calendarActDate" tal:content="php:utf8_encode(ucfirst(strftime('%A %e %B %Y',eventE[0]['Tevent']['date'])))" /> 
+//<span class="calendarActDate" tal:content="php:utf8_encode(ucfirst(strftime('%A %e %B %Y',eventE[0]['Tevent']['date'])))" />
 $currentStarDate = number_format((((date("Y",$curTime)+$bounceYear)-2323) + (date("z",$curTime)/365.2425))*1000 + ((date("H",$curTime)*3600 + date("i",$curTime)*60 + date("s",$curTime)) * $lapConstant),2,".","");
 
 ?>

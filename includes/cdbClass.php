69cbd722cf232b5db06dc5ae19e77a727b2bf8d9
Azufre451 <moreno.zolfo@gmail.com>
2021-02-08 23:10:03 +0100

    Remove old unecessary code from database and cdb

--- includes/cdbClass.php
+++ includes/cdbClass.php
@@ -2,7 +2,7 @@
 class CDB
 {
 
-	public static $allowedHTML = '<table><p><span><tr><td><div><hr><tbody><img><a><li><ul><ol><blockquote><b><u><i><font><sup><sub>';
+	public static $allowedHTML = '<table><p><span><tr><td><div><hr><tbody><img><a><li><ul><ol><blockquote><b><u><i><font><sup><sub><br>';
 
 
 	public static function mergeTopics($t1,$t2)

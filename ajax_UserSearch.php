432112f3c21a2bd08da1ef08aa7793a42651b665
jean-n92 <g.genetlici@gmail.com>
2021-04-03 02:13:36 +0200

    Added draft for user research feature

--- ajax_UserSearch.php
+++ ajax_UserSearch.php
@@ -5,15 +5,24 @@ if (!isSet($_SESSION['pgID'])){header("Location:login.php"); exit;}
 include('includes/app_include.php');
 $disc=(isSet($_GET['filter'])) ? $_GET['filter'] : '';
 $term = addslashes(@$_GET['term']);
-
-if($disc=="PNG")
-$res = mysql_query("SELECT pgUser FROM pg_users WHERE png=1 AND pgUser LIKE '$term%'");
-else if($disc == "PREST") $res= mysql_query("SELECT pgUser FROM pg_users WHERE pgID <> '".($_SESSION['pgID'])."' AND LOWER(pgOffAvatarC) = '".addslashes(strtolower($_POST['term2']))."' AND LOWER(pgOffAvatarN) = '".addslashes(strtolower($_POST['term1']))."'");
-else $res = mysql_query("SELECT pgUser FROM pg_users WHERE pgUser LIKE '$term%'");
-
 $aar = array();
-while ($row = mysql_fetch_array($res)) {
-$aar[] = $row['pgUser'];
+
+if($disc=="PID"){
+    $res = mysql_query("SELECT pgUser AS value, pgID AS PUD FROM pg_users WHERE pgUser LIKE '$term%' LIMIT 20");
+    while($row = mysql_fetch_array($res)) {
+        $aar[] = $row;
+    }
+}
+else {
+    if($disc=="PNG")
+    $res = mysql_query("SELECT pgUser FROM pg_users WHERE png=1 AND pgUser LIKE '$term%' LIMIT 20");
+    
+    else if($disc == "PREST") $res= mysql_query("SELECT pgUser FROM pg_users WHERE pgID <> '".($_SESSION['pgID'])."' AND LOWER(pgOffAvatarC) = '".addslashes(strtolower($_POST['term2']))."' AND LOWER(pgOffAvatarN) = '".addslashes(strtolower($_POST['term1']))."'");
+    
+    else $res = mysql_query("SELECT pgUser FROM pg_users WHERE pgUser LIKE '$term%' LIMIT 20");
+    while ($row = mysql_fetch_array($res)) {
+    $aar[] = $row['pgUser'];
+    }
 }
 echo json_encode($aar);
 //echo var_dump($aar);

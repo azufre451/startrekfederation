9966f411f8c0f20c23fae06587b296f0624e8cc4
Azufre451 <moreno.zolfo@gmail.com>
2021-04-10 20:47:57 +0200

    fix bug in stardate calculation in CDB

--- ajax_stardateGetter.php
+++ ajax_stardateGetter.php
@@ -7,12 +7,12 @@ if (!isSet($_SESSION['pgID'])){header("Location:login.php"); exit;}
 include('includes/app_include.php');
 include('includes/validate_class.php'); 
 
-$curTime = mktime($_POST['hou'],0,$_POST['min'],$_POST['mon'],$_POST['day'],$_POST['yea']);		
+$curTime = mktime($_POST['hou'],$_POST['min'],0,$_POST['mon'],$_POST['day'],$_POST['yea']-$bounceYear);
 
 $lapConstant = ((date("Y",$curTime) % 4) == 0) ? 0.00003162315 : 0.00003170979; 
 //$currentDate = date("d/m",$curTime)."/".(date("Y",$curTime)+368);
-$currentStarDate = number_format((((date("Y",$curTime))-2323) + (date("z",$curTime)/365.2425))*1000 + ((date("H",$curTime)*3600 + date("i",$curTime)*60 + date("s",$curTime)) * $lapConstant),2,".","");
+$currentStarDate = number_format((((date("Y",$curTime))+$bounceYear-2323) + (date("z",$curTime)/365.2425))*1000 + ((date("H",$curTime)*3600 + date("i",$curTime)*60 + date("s",$curTime)) * $lapConstant),2,".","");
 
  echo json_encode($currentStarDate);
-
 ?>
+
